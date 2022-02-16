091193b47116607ff52ae00a205ba0ea4847dcd0
drunk喵咪 <1684838553@qq.com>
2022-02-16 15:17:56 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ContextComponent from './components/useContext'
+
+function App() {
+  return (
+    <div className="App">
+      <ContextComponent />
+    </div>
+  );
+}
+
+export default App;

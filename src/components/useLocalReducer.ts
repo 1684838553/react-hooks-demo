ee8b8e914f46582c53a76711c597c9b8a6896fcb
drunk喵咪 <1684838553@qq.com>
2022-02-16 15:47:29 +0800

    Create useLocalReducer.ts

---
+++ src/components/useLocalReducer.ts
@@ -0,0 +1,31 @@
+import React, { useReducer, useState } from 'react'
+import './style.css'
+
+const store = {
+    num: 10  // 初始值
+}
+
+const reducer = (state: any, action: any) => {
+    switch (action.type) {
+        case 'changeNum':
+            return {
+                ...state,
+                num: action.num
+            }
+        case 'newObject':
+            return {
+                ...state,
+                object: action.object
+            }
+        default:
+            return {
+                ...state
+            }
+    }
+}
+
+export default function useLocalReducer() {
+    const [state, dispatch] = useReducer(reducer, store)
+
+    return [state, dispatch]
+}

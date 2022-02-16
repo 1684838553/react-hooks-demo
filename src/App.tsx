c555bc6e30810419277ce37f12f8a219054fdeba
drunk喵咪 <1684838553@qq.com>
2022-02-16 15:44:56 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import ContextComponent from './components/useContext'
+import ReducerComponent from './components/useReducer'
 
 function App() {
   return (
     <div className="App">
       <ContextComponent />
+      -----------
+      <ReducerComponent />
     </div>
   );
 }
